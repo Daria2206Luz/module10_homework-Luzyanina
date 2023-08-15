50119660d7275d43f05520b9c40118553e8a1fbc
Daria <dashalu123@yandex.ru>
2023-08-15 12:42:22 +0300

    changes

--- task1.js
+++ task1.js
@@ -1,1 +1,10 @@
-const answer2 = prompt('Как тебя зовут?', 'Имя')
+const result = +prompt("Введите число", "");
+if (typeof +result !== "number") {
+  alert("Упс, кажется, вы ошиблись");
+} else if (Number.isNaN(result % 2)) {
+  alert("Ошибка, введено не число");
+} else if (result % 2 === 0) {
+  alert("Число четное");
+} else {
+  alert("Число нечетное");
+}
---
+++ task2.js
@@ -0,0 +1,51 @@
+let x;
+x = 2;
+
+if (typeof x == "number") {
+  console.log("x - число");
+} else if (typeof x == "string") {
+  console.log("x - сторка");
+} else if (typeof x == "boolean") {
+  console.log("x - логический тип");
+} else {
+  console.log("Тип x не определён");
+}
+
+let x1;
+x1 = true;
+
+if (typeof x1 == "number") {
+  console.log("x - число");
+} else if (typeof x1 == "string") {
+  console.log("x - сторка");
+} else if (typeof x1 == "boolean") {
+  console.log("x - логический тип");
+} else {
+  console.log("Тип x не определён");
+}
+
+let x2;
+x2 = "SkillFactory";
+
+if (typeof x2 == "number") {
+  console.log("x - число");
+} else if (typeof x2 == "string") {
+  console.log("x - сторка");
+} else if (typeof x2 == "boolean") {
+  console.log("x - логический тип");
+} else {
+  console.log("Тип x не определён");
+}
+
+let x3;
+x3 = null;
+
+if (typeof x3 == "number") {
+  console.log("x - число");
+} else if (typeof x3 == "string") {
+  console.log("x - сторка");
+} else if (typeof x3 == "boolean") {
+  console.log("x - логический тип");
+} else {
+  console.log("Тип x не определён");
+}
