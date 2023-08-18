f7b2f9165974a39a9c41eca16a886edb50f7e87d
Daria <dashalu123@yandex.ru>
2023-08-18 20:34:50 +0300

    changes

---
+++ task4.js
@@ -0,0 +1,7 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(100);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomIntInclusive());
