437fa8ac34eab6c17abdfb482a9bc2ad381c7276
Daria <dashalu123@yandex.ru>
2023-09-21 01:47:00 +0300

    changes

--- task8.js
+++ task8.js
@@ -1,8 +1,8 @@
+let myMap = new Map();
+myMap.set("гемоглобин", "клинический анализ крови");
+myMap.set("холестерин", "биохимический анализ крови");
+myMap.set("цитология", "мазок");
 
-
-    
-  
-
-  
-
-
+for (let name of myMap.keys()){
+console.log("Ключ - " + name + "," + " Значение - " + myMap.get(name));
+}
